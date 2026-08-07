a133fa45ba50bd871fbec8a9b9a93963be6a63c8
Melih <melih.ckmakk@gmail.com>
2026-08-07 16:39:02 +0300

    Çözümüm Eklendi.

---
+++ LeetCodeÇalışmalarım/0179-Largest Nımber/Solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def largestNumber(self, nums: list[int]) -> str:
+        
+        metin_sayilar = []
+        for sayi in nums:
+            metin_sayilar.append(str(sayi))        #Input: nums = [3,30,34,5,9]
+                                                   #Output: "9534330"
+        kac_sayi_var = len(metin_sayilar)       
+        for i in range(kac_sayi_var):
+            for j in range(i + 1, kac_sayi_var):
+                
+                ihtimal1 = metin_sayilar[i] + metin_sayilar[j]
+                ihtimal2 = metin_sayilar[j] + metin_sayilar[i]
+                
+                if ihtimal2 > ihtimal1:
+                    metin_sayilar[i], metin_sayilar[j] = metin_sayilar[j], metin_sayilar[i]
+                    
+        sonuc = ""
+        for kelime in metin_sayilar:
+            sonuc += kelime
+            
+        if sonuc[0] == "0":
+            return "0"
+            
+        return sonuc
